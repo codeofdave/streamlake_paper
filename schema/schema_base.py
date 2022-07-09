9719b7e5d4057eea5c7b8d224e1b7b67d9d37362
dave <734388421@qq.com>
2022-07-09 20:56:21 +0800

    add dpi data sample

--- schema/schema_base.py
+++ schema/schema_base.py
@@ -3,7 +3,7 @@ import csv
 class SchemaBase:
     def __init__(self):
         pass
-    def get_schema(self, file_path=None, file_type='CSV'):
+    def get_schema(self, file_path=None, file_type='xlsx'):
         with open(file_path,'r') as file:
 
 
